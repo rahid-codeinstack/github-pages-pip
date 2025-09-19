8287438b58229f79a7776504df842cb1f5c75004
rahidkhan <rahidkhanbannu1@email.com>
2025-09-19 07:05:09 +0430

    clear react defaul ui "

--- src/config.test.js
+++ src/config.test.js
@@ -1,6 +1,17 @@
-import { expect , test  } from "vitest"
+import { expect , test , expectTypeOf } from "vitest"
 import { sum } from "./confige"
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
+  expectTypeOf({a:1}).toEqualTypeOf({a:1})
+  expectTypeOf({a:1,b:2}).toMatchObjectType({a:1});
+
+})
+test('to mathcing ' ,()=>{
+     expectTypeOf({a:2}).toEqualTypeOf({a:2})
+})
+test('to not equal ',()=>{
+     expectTypeOf({myname:"rahidkhan"}).toBeObject()
+     expectTypeOf({ a: 1, b: 1 }).toMatchTypeOf({ a:1 })
+     expectTypeOf({a:1}).not.toEqualTypeOf({a:"1"})
 })
