8bd46c4a7f29c4513b5e8d435c4f5830060a24c5
rahidkhan <rahidkhanbannu1@email.com>
2025-09-19 07:18:18 +0430

    create box ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import './App.css'
 
 function App(){
   const [count , setcount ] =  useState(0);
-  return <h1>hellow world this is the github-pagea-project with devop integration</h1>
+  return  <div className="container">
+    <h1 className='h1'>
+        <span>github pages project </span> 
+        <span>i am rahdkhan </span>
+        <span>this is my first pip to github</span>
+    </h1>
+  </div>
 }
 
 export default App
